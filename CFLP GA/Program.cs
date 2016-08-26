2177b0941503bd35901f7abe7ca97927de989ef9
manic-milos <manic.milos@gmail.com>
2016-08-26 02:48:54 +0200

    First results

--- CFLP GA/Program.cs
+++ CFLP GA/Program.cs
@@ -13,11 +13,12 @@ namespace CFLP_GA
         static void Main(string[] args)
         {
 
+            Random r = new Random(2);
             for (int i = 0; i < 30; i++)
             {
                 Execution_Reports.ReportController.reportPrefix = i + "_";
-                Random r = new Random(2);
                 ControlledRandom.reset(r.Next());
+                ControlledRandom.reset();
                 TestOnData test = new TestOnData();
                 test.testSelectOnFolder(@"D:\reinstalacija\F\Projects\ConsoleApplication8\ConsoleApplication8\FormatedInstances",
                     true, true, true, true);
