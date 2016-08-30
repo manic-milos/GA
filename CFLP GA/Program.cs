288f6caa50c3138628a05da7bdd6e3349fdf1030
manic-milos <manic.milos@gmail.com>
2016-08-31 01:01:07 +0200

    18th iteration

--- CFLP GA/Program.cs
+++ CFLP GA/Program.cs
@@ -12,9 +12,14 @@ namespace CFLP_GA
     {
         static void Main(string[] args)
         {
-
+            int starting_iteration = 5;
             Random r = new Random(2);
-            for (int i = 0; i < 30; i++)
+            for (int i = 0; i < starting_iteration; i++)
+            {
+                ControlledRandom.reset(r.Next());
+                ControlledRandom.reset();
+            }
+            for (int i = starting_iteration; i < 30; i++)
             {
                 Execution_Reports.ReportController.reportPrefix = i + "_";
                 ControlledRandom.reset(r.Next());
